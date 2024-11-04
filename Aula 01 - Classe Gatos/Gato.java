7ae333b4c6323aa614626df7911d85c6a6224849
cecilia-ionara <128868521+cecilia-ionara@users.noreply.github.com>
2024-11-04 12:09:43 -0300

    Add files via upload

--- Aula 01 - Classe Gatos/Gato.java
+++ Aula 01 - Classe Gatos/Gato.java
@@ -6,5 +6,26 @@ public class Gato {
 	public Integer qtdComidaDisponivel; //usar int(primitivo) ou Integer(complexo), melhor Integer
 	public Boolean sono;
 	
+	//COSNTRUTOR = usando um metodo para criar gatos de acordo com seus atributos
+	//public <class>(tipos e atributos)
+	public Gato(String nome, Integer comida, Boolean sono) {
+		//this.nome = "Sushi"; //esse se refere a classe dentro dessas chaves SEMPRE USAR THIS
+		this.nome = nome;
+		this.qtdComidaDisponivel = comida;
+		this.sono = sono;
+	}
 	
+	//MÉTODOS, FUNÇÕES,AÇÕES
+	//void pq nao retorna nada
+	public void miar() {
+		System.out.println("Miau!!");
+	}
+	
+	public void comer() {
+		this.qtdComidaDisponivel -= this.qtdComidaDisponivel; //tira um quando ele comer
+	}
+	
+	public void dormir() {
+		this.sono = false;
+	}
 }
