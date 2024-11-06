f8756e4a4dfaf9b35a76a31c72f4270e65d089ce
cecilia-ionara <128868521+cecilia-ionara@users.noreply.github.com>
2024-11-06 09:26:03 -0300

    Add files via upload

--- aula02/Conta.java
+++ aula02/Conta.java
@@ -5,6 +5,7 @@ public class Conta {
 	public Double saldo;
 	public Double limite;
 	public Cliente titular;
+	public static final Double limiteMaximo = 100.0;
 	
 	public Conta(Integer numero, Double saldo, Double limite, Cliente titular) {
 		this.numero = numero;
@@ -20,15 +21,44 @@ public class Conta {
 	}
 	
 	public Boolean sacar(Double quantidade) {
-		if (quantidade > this.saldo)
+		if (quantidade > 0.0) {
+			if (quantidade <= this.saldo) {
+				this.saldo -= quantidade; 
+				return true;	
+			} else if (quantidade <=  this.saldo + this.limite) {
+				quantidade -= this.saldo;
+				this.saldo = 0.0;
+				this.limite -= quantidade;
+				return true;
+			
+			} else 
+				return false;
+		} else 
 			return false;
-		else {
-		this.saldo = this.saldo - quantidade;
-			return true;
 	}
-}
 	
-	public void depositar(Double quantidade) {
-		this.saldo = this.saldo + quantidade;
+	public Boolean depositar(Double quantidade) {
+		if (quantidade > 0) {
+			if (this.limite < Conta.limiteMaximo) {
+				Double diferenca = Conta.limiteMaximo - this.limite;
+				
+				if (diferenca < quantidade) {
+					this.limite = Conta.limiteMaximo;
+					quantidade -= diferenca;
+					this.saldo += quantidade;
+					return true;
+			
+				}
+			} else
+					this.limite += quantidade;
+					return true;
+		} else 
+			return false;
+	}
+	
+	public Boolean transfere(Double valor, Conta destino) {
+		this.sacar(valor);
+		destino.depositar(valor);
+		return true;
 	}
 }
--- aula02/TestadorDeContas.java
+++ aula02/TestadorDeContas.java
@@ -5,14 +5,19 @@ public class TestadorDeContas {
 		
 		Cliente cl1 = new Cliente("Cecília", "Ionara", "042.701.612-64");
 		
-		Conta c1 = new Conta(12, 2000.2, cl1);
+		Conta c1 = new Conta(1200, 2000.2, cl1);
+		
+		Cliente cl2 = new Cliente("Maria", "Alice", "042.701.612-64");
+		
+		Conta c2 = new Conta(1000, 2000.2, cl2);
 		
 		
-		c1.sacar(150.00);
-		System.out.println(c1.saldo);
+		c2.transfere(100.0, c1);
 		
-		c1.depositar(200.00);
-		System.out.println(c1.saldo);
+		System.out.print("Saldo C1: " + c1.saldo);
+		System.out.print("Limite C1: " + c1.limite);
 		
+		System.out.print("Saldo C2: " + c2.saldo);
+		System.out.print("Limite C2: " + c2.limite);
 		}
 }
